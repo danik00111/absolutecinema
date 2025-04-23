b46ea28cd7d342235c00e377de8358cddccd5210
danik00111 <kremenskiydaniil@gmail.com>
2025-04-23 19:03:47 +0300

    remove dashes

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename='/absolute-cinema'>
+    <BrowserRouter basename='/absolutecinema'>
       <App />
     </BrowserRouter>
   </StrictMode>
